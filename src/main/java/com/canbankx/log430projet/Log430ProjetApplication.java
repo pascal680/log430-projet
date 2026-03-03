efe9b6f2eb18f5b1aaece479e5e29d43bea49291
Pascal Bourgoin <pascal680@hotmail.ca>
2026-03-03 02:05:44 -0500

    preparation du projet log430 et ajout de dépendances

---
+++ src/main/java/com/canbankx/log430projet/Log430ProjetApplication.java
@@ -0,0 +1,13 @@
+package com.canbankx.log430projet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Log430ProjetApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Log430ProjetApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/canbankx/log430projet/Log430ProjetApplicationTests.java
@@ -0,0 +1,13 @@
+package com.canbankx.log430projet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Log430ProjetApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
